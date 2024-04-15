f68338bc87581e27a845e3e94243a96306c9870d
tu <tu734843818@yahoo.com>
2024-04-15 19:36:43 +0800

    [nvim] project quick-scope

---
+++ nvim/lua/c-neovide.lua
@@ -0,0 +1,3 @@
+if vim.g.neovide then
+	vim.o.guifont = "Source Code Pro:h25" -- text below applies for VimScript
+end
--- nvim/lua/lsp-rust.lua
+++ nvim/lua/lsp-rust.lua
@@ -1,6 +1,32 @@
 require("lspconfig").rust_analyzer.setup({})
 
 local rt = require("rust-tools")
+-- Update this path
+local extension_path = "/home/tu/.vscode-oss/extensions/vadimcn.vscode-lldb-1.10.0/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = extension_path .. "lldb/lib/liblldb"
+local this_os = vim.loop.os_uname().sysname
+
+-- The path in windows is different
+if this_os:find("Windows") then
+	codelldb_path = extension_path .. "adapter\\codelldb.exe"
+	liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+else
+	-- The liblldb extension is .so for linux and .dylib for macOS
+	liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+end
+-- Normal setup
 rt.setup({
-	server = {},
+	server = {
+		on_attach = function(_, bufnr)
+			-- Hover actions
+			-- vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+			-- Code action groups
+			-- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+			vim.keymap.set("n", "<Leader>rd", "<cmd>w<cr> <cmd>RustRunnable<cr> 1", { buffer = bufnr })
+		end,
+	},
+	dap = {
+		adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+	},
 })
