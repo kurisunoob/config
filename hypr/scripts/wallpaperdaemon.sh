c357547a680bac30e1a1cb873d6230f57c6123db
tu <tu734843818@yahoo.com>
2025-07-08 20:06:02 +0800

    [hypr] config wallpaper-select

--- hypr/scripts/wallpaperdaemon.sh
+++ hypr/scripts/wallpaperdaemon.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-swww query
+# 检查 swww 守护进程是否运行
+swww query >/dev/null 2>&1
 if [ $? -eq 1 ]; then
-  swww init
-
-  swww img ~/.config/swww/current.set \
-    --transition-type "wipe" \
-    --transition-duration 2
+  # 启动 swww-daemon 并在后台运行
+  swww-daemon &
+  
+  # 等待守护进程初始化（避免竞争条件）
+  sleep 1
+  
+  # 检查壁纸文件是否存在
+  WALLPAPER="$HOME/.config/swww/current.set"
+  if [ -f "$WALLPAPER" ]; then
+    swww img "$WALLPAPER" \
+      --transition-type "wipe" \
+      --transition-duration 2
+  else
+    echo "Error: Wallpaper file $WALLPAPER does not exist."
+    exit 1
+  fi
+else
+  echo "swww-daemon is already running."
 fi
--- hypr/scripts/wallpaperselect.sh
+++ hypr/scripts/wallpaperselect.sh
@@ -12,13 +12,12 @@ x_monres=$(( x_monres * 17 / monitor_scale ))
 
 # set rofi override
 elem_border=$(( hypr_border * 3 ))
-r_override="element{border-radius:${elem_border}px;} listview{columns:6;spacing:100px;} element{padding:0px;orientation:vertical;} element-icon{size:${x_monres}px;border-radius:0px;} element-text{padding:20px;}"
-
+r_override="element{border-radius:${elem_border}px;} listview{columns:6;lines:2;spacing:10px;} element{padding:0px;orientation:vertical;} element-icon{size:${x_monres}px;border-radius:0px;} element-text{padding:20px;}"
 # launch rofi menu
 RofiSel=$( find -L "${wallPath}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort | while read rfile
 do
   echo -en "$rfile\x00icon\x1f${wallPath}/${rfile}\n"
-done | rofi -dmenu -theme-str "${r_override}" -config "${RofiConf}" -select "${currentWall}")
+done | rofi -show-icons -dmenu -theme-str "${r_override}" -config "${RofiConf}" -select "${currentWall}")
 
 # apply wallpaper
 if [ ! -z "${RofiSel}" ] ; then
